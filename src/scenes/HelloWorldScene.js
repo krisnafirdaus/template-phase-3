f7a19226875d0432cf5073426421eafe86df53b9
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-01-18 19:41:20 +0800

    initial project

---
+++ src/scenes/HelloWorldScene.js
@@ -0,0 +1,12 @@
+import Phaser from 'phaser'
+export default class HelloWorldScene extends Phaser.Scene
+   {
+{
+ constructor()
+    }
+init(){      }
+ 
+  preload() {}
+create() {
+}update(){ }
+}
